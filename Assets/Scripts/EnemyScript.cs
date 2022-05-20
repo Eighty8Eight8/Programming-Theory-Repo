ca5aa6d85f86cc2e197f03db768c9918e061f509
Eighty8Eight8 <103387122+Eighty8Eight8@users.noreply.github.com>
2022-05-20 12:44:20 +0300

    main scene
    - added enemy

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    private Rigidbody enemyRB;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        enemyRB = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        float speed = 2.5f;
+        Vector3 moveDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRB.AddForce(moveDirection * speed, ForceMode.Impulse);
     }
 }
